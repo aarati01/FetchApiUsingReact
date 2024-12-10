f8813b0654b5977270874ed52cc68a319caf4b82
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-10 01:10:58 -0500

    data fetch properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react"
 
 const App = () => {
   const[cards,setCards]=useState([])
 
   const fetchData=async()=>{
-    
+    let a =await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data =await a.json()
+    console.log(data)
   }
+  useEffect(()=>{
+    fetchData()
+
+  },[])
   return (
     <>
+    <div className="container">
+      <div className="card"></div>
+    </div>
      </>
   )
 }
