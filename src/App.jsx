ef9a65899c3ce3686469ebd8fa9a5dbc448a2582
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-10 01:24:43 -0500

    new filder components os added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react";
+
 const App = () => {
-  const[cards,setCards]=useState([])
+  const [cards, setCards] = useState([]);
 
-  const fetchData=async()=>{
-    let a =await fetch("https://jsonplaceholder.typicode.com/posts")
-    let data =await a.json()
-    console.log(data)
-  }
-  useEffect(()=>{
-    fetchData()
+  const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let data = await a.json();
+    console.log(data);
+    setCards(data);  // Update the state with fetched data
+  };
 
-  },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-    <div className="container">
-      {cards.map((card)=>{
-        return  <div  key={card.id}className="card">
-          <h1>{card.title}</h1>
-          <p>{card.body}</p>
-          <span>By:UserId:{card.userId}</span>
-                </div>
+      <div className="container">
+        {cards.map((card) => {
+          return (
+            <div key={card.id} className="card">
+              <h1>{card.title}</h1>
+              <p>{card.body}</p>
+              <span>By: UserId: {card.userId}</span>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-      })}
-
-    </div>
-     </>
-  )
-}
-
-export default App
+export default App;
