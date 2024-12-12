149bf999bedb53eacdc11fc08cc4ec1a774a60bc
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-12 16:32:52 -0500

    form input is taken

--- src/assets/components/EventHandling.jsx
+++ src/assets/components/EventHandling.jsx
@@ -2,16 +2,19 @@ import { useState } from "react"
 
 // import { useState } from "react"
   function EventHandling(){
-    const [name,setname]=useState("aarati");
+    // const [name,setname]=useState("aarati");
+  const [Form, setForm] = useState({});
 
 const EventHandling2 = () => {
     alert("Hey I am clicked!!")
 }
-const handleMouseOver=()=>{
-    alert ("Hey I am a mouse Over")
-}
+// const handleMouseOver=()=>{
+//     alert ("Hey I am a mouse Over")
+// }
 const handleChnage=(e)=>{
-  setname(e.target.value)
+  // setName(e.target.value)
+  setForm({...Form,[e.target.name]:e.target.value})
+  console.log(Form)
 }
 
   return (
@@ -19,10 +22,12 @@ const handleChnage=(e)=>{
     <div className="button">
         <button onClick={EventHandling2}>Click me</button>
     </div>
-    <div className="red" onMouseOver={handleMouseOver}>
+    {/* <div className="red" onMouseOver={handleMouseOver}>
         I am a red Dev
-    </div>
-    <input type="text" value={name} onChange={handleChnage}/>
+    </div> */}
+    <input type="text" name="email" value={Form.email?Form.email:""} onChange={handleChnage}/>
+    <input type="text"  name="phone"value={Form.phone?Form.phone:""} onChange={handleChnage}/>
+
     </>
   )
 
