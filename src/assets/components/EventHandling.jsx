d69c388c8c6c77cd1150dfd7f9a83e35b99fa2ad
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-12 11:37:18 -0500

    Event handeling is added

---
+++ src/assets/components/EventHandling.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react"
+  function EventHandling(){
+
+const EventHandling2 = () => {
+    alert("Hey I am clicked!!")
+}
+  return (
+    <>
+    <div className="button">
+        <button onClick={EventHandling2}>Click me</button>
+    </div>
+    </>
+  )
+
+
+  }
+export default EventHandling
