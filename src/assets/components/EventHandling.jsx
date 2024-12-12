aef4ed287bca523cd816db55fd8bb3bc194cae65
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-12 16:18:33 -0500

    input type is added

--- src/assets/components/EventHandling.jsx
+++ src/assets/components/EventHandling.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
+
 // import { useState } from "react"
   function EventHandling(){
+    const [name,setname]=useState("aarati");
 
 const EventHandling2 = () => {
     alert("Hey I am clicked!!")
 }
 const handleMouseOver=()=>{
     alert ("Hey I am a mouse Over")
+}
+const handleChnage=(e)=>{
+  setname(e.target.value)
 }
 
   return (
@@ -16,9 +22,10 @@ const handleMouseOver=()=>{
     <div className="red" onMouseOver={handleMouseOver}>
         I am a red Dev
     </div>
+    <input type="text" value={name} onChange={handleChnage}/>
     </>
   )
 
 
-  }
+}
 export default EventHandling
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import EventHandling from './assets/components/EventHandling'
-// import App from './App.jsx'
+import EventHandling from './assets/components/EventHandling.jsx'
 
 
 createRoot(document.getElementById('root')).render(
