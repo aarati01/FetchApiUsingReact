00fbbb68629fd89f29946720dd19e8ba055231a5
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-12 11:42:28 -0500

    mouserOver is added

--- src/assets/components/EventHandling.jsx
+++ src/assets/components/EventHandling.jsx
@@ -4,10 +4,17 @@
 const EventHandling2 = () => {
     alert("Hey I am clicked!!")
 }
+const handleMouseOver=()=>{
+    alert ("Hey I am a mouse Over")
+}
+
   return (
     <>
     <div className="button">
         <button onClick={EventHandling2}>Click me</button>
+    </div>
+    <div className="red" onMouseOver={handleMouseOver}>
+        I am a red Dev
     </div>
     </>
   )
